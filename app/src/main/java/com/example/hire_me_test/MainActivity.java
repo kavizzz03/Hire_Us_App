722d8c55c57fe742124cadc7547e585579e2409a
kavindu_bogahawatte <kavindumalshan2003@gmail.com>
2025-06-07 16:56:09 +0530

    first commit

--- app/src/main/java/com/example/hire_me_test/MainActivity.java
+++ app/src/main/java/com/example/hire_me_test/MainActivity.java
@@ -1,24 +1,28 @@
 package com.example.hire_me_test;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.cardview.widget.CardView;
 
 public class MainActivity extends AppCompatActivity {
+
+    CardView findJobLayout, giveJobLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        findJobLayout = findViewById(R.id.findJobLayout);
+        giveJobLayout = findViewById(R.id.giveJobLayout);
+
+        findJobLayout.setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this, FindJobActivity.class));
+        });
+
+        giveJobLayout.setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this, GiveJobActivity.class));
         });
     }
 }
